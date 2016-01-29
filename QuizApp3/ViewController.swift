3f445d95ceacdfd45404a1a44be6ce11c5b1c3b3
高木則迪 <dc667_tg@yahoo.co.jp>
2016-01-29 11:21:43 +0900

    Initial Commit

---
+++ QuizApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizApp3
+//
+//  Created by Norimichi Takagi on 2016/01/29.
+//  Copyright © 2016年 NorimichiTakagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
